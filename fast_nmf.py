5eaaf55de6be9ce6268b5f656bf2f49503500a06
Louis Lalay <louis.lalay@ens-paris-saclay.fr>
2023-01-18 16:19:47 +0100

    Random Init OK

--- fast_nmf.py
+++ fast_nmf.py
@@ -1,6 +1,17 @@
 from numpy import typing
+import numpy as np
+from matplotlib import pyplot as plt
+import unittest
 
-def init_fast_MNMF(init_type: str) -> tuple:
+
+def init_fast_MNMF(
+    init_type: str,
+    nFFT: int,
+    nTimeFrames: int,
+    n_bases: int,
+    n_sources: int,
+    n_sensors: int,
+) -> tuple:
     init_type_dict = {"RANDOM": 0, "DIAGONAL": 1, "CIRCULAR": 2, "GRADUAL": 3}
     try:
         ind = init_type_dict[init_type.upper()]
@@ -10,16 +21,25 @@ def init_fast_MNMF(init_type: str) -> tuple:
     match ind:
         case 0:
             # Random init
-            W, H, G_tilde, Q = ...
+
+            # G_tilde is a vector of size M = n_sensors
+            G_tilde = np.random.rand(n_sources, n_sensors)
+            # Q is nFFTxMxM
+            Q = np.random.rand(nFFT, n_sensors, n_sensors)
         case 1:
             # Diagonal init
-            W, H, G_tilde, Q = ...
+            G_tilde = ...
+            Q = np.identity(n_sensors)
         case 2:
             # Circular init
-            W, H, G_tilde, Q = ...
+            G_tilde = ...
+            Q = ...
         case 3:
             # Gradual init
-            W, H, G_tilde, Q = ...
+            G_tilde = ...
+            Q = ...
+    W = np.random.rand(nFFT, n_bases)
+    H = np.random.rand(n_bases, nTimeFrames)
     return W, H, G_tilde, Q
 
 
@@ -64,19 +84,61 @@ def log_likelihood(
     Q: typing.ArrayLike,
 ) -> float:
     """This function computes the log likelihood of FastMNMF2"""
-    log_like = ...
+    log_like = 10
     return log_like
 
 
-def fast_MNMF(X: typing.ArrayLike, init_type: str = "RANDOM"):
-    """This function """
+def fast_MNMF(
+    X: typing.ArrayLike, init_type: str = "RANDOM", show: bool = True
+) -> tuple:
+    """Input: STFT of a mixture of N sources recorded by M microphones
+    Output: W, H, G, Q optimized by maximum likelihood"""
+
+    # Number of itteration. Maybe replace it later by convergence criteria
+    n_itteration = 50
+
+    # Initialization
     W, H, G_tilde, Q = init_fast_MNMF(init_type)
-    return
+    log_like_list = np.zeros(n_itteration)
+
+    # Loop
+    for k in range(n_itteration):
+        W, H, G_tilde, Q = update_all_params(W, H, G_tilde, Q)
+        log_like_list[k] = log_likelihood(X, W, H, G_tilde, Q)
+    if show:
+        fig = plt.figure(figsize=(20, 10))
+        ax = fig.gca()
+        ax.plot(log_like_list)
+        plt.show()
+    return W, H, G_tilde, Q
+
+
+def source_estimation(
+    X: typing.ArrayLike,
+    W: typing.ArrayLike,
+    H: typing.ArrayLike,
+    G_tilde: typing.ArrayLike,
+    Q: typing.ArrayLike,
+) -> typing.ArrayLike:
+    """Input: MNMF matrices
+    Output: Separated source images. source_images[n] is the spectrogram of estimated n^th source"""
+    source_images = ...
+    return source_images
 
 
 def main():
-    test()
+    X = np.zeros((1025, 100, 4))
+    W, H, G_tilde, Q = fast_MNMF(X)
+    source_images = source_estimation(W, H, G_tilde, Q)
     return
+
+
+class FastNMFtest(unittest.TestCase):
+    def setUp(self):
+        ...
+
+    def test_default_widget_size(self):
+        self.assertEqual()  # test , # expected value, # message
 
 
 if __name__ == "__main__":
